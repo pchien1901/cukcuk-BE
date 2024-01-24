740a2ebe5bb3413005c9c2d8a302a58f92197895
pchien1901 <phamminhchien1901@gmail.com>
2024-01-24 08:36:00 +0700

    debug report

---
+++ core/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using core.Entities;
+using MISA.CUKCUK.Core.DTOs.ImportDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.CUKCUK.Core.AutoMapper
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CustomerImport, Customer>();
+        }
+    }
+}
---
+++ respository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using core.Entities;
+using MISA.CUKCUK.Core.Interfaces;
+using MISA.CUKCUK.Core.Services;
+using MISA.CUKCUK.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.CUKCUK.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        IMISADbContext _dbContext;
+        public ICustomerRepository Customers { get; set; }
+        public ICustomerGroupRepository CustomerGroups { get; set; }
+        public IEmployeeRepository Employees { get; set; }
+        bool _disposed;
+
+        public UnitOfWork(IMISADbContext dbContext, ICustomerRepository customerRepository, ICustomerGroupRepository customerGroups, IEmployeeRepository employees)
+        {
+            _dbContext = dbContext;
+            Customers = customerRepository;
+            CustomerGroups = customerGroups;
+            Employees = employees;
+        }
+
+        public void BeginTransaction()
+        {
+            _dbContext.Connection.Open();
+            _dbContext.Transaction = _dbContext.Connection.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            if(_dbContext.Transaction != null)
+            {
+                _dbContext.Transaction.Commit();
+                Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            if(_dbContext.Connection != null)
+            {
+                _dbContext.Connection.Dispose();
+            }
+        }
+
+        public void Rollback()
+        {
+            if(_dbContext.Transaction != null)
+            {
+                _dbContext.Transaction.Rollback();
+                Dispose();
+            }
+        }
+    }
+}
